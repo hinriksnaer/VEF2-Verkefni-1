d920c6a96f709da5665f83937e36f05fc200bf45
hinriksnaer <hinriksnaer@gmail.com>
2018-02-04 16:14:20 +0000

    first push, majority of functionality added

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const router = require('./articles');
 
 const app = express();
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.use('/img', express.static(path.join(__dirname, 'articles/img/')));
+app.set('view engine', 'ejs');
+
+function notFoundHandler(req, res) {
+  res.status(404).render('error', {
+    title: 'Villa',
+    headerText: 'Fannst ekki',
+    errorText: 'Ó nei, efnið fannst ekki',
+  });
+}
+
+function errorHandler(req, res) {
+  res.status(500).render('error', {
+    title: 'Villa',
+    headerText: 'Villa kom upp',
+    errorText: '',
+  });
+}
+
+app.use('/', router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
--- articles.js
+++ articles.js
@@ -1,1 +1,81 @@
-/* útfæra greina virkni */
+const fs = require('fs');
+const marked = require('marked');
+const express = require('express');
+
+const router = express.Router();
+const util = require('util');
+const fm = require('front-matter');
+
+const errorLog = [];
+
+function getSlugList(data) {
+  const slugList = [];
+  for (let i = 0; i < data.length; i += 1) {
+    slugList.push(data[i].attributes.slug);
+  }
+  return slugList;
+}
+
+const getFilesAsync = async () => {
+  const files = await util.promisify(fs.readdir)('./articles');
+  return Promise.all(files.map(async (file) => {
+    try {
+      return fm(await util.promisify(fs.readFile)(`./articles/${file}`, 'utf8'));
+    } catch (err) {
+      errorLog.push(err);
+      return undefined;
+    }
+  }));
+};
+router.get('/', async (req, res) => {
+  let data = await getFilesAsync();
+  data = data.filter(x => x);
+  const dataList = [];
+  for (let i = 0; i < data.length; i += 1) {
+    dataList.push(data[i].attributes);
+    dataList[i].date = new Date(dataList[i].date);
+  }
+  dataList.sort((a, b) => b.date - a.date);
+  const articleTitle = [];
+  const articleDate = [];
+  const articleSlug = [];
+  const articleImg = [];
+
+  for (let i = 0; i < data.length; i += 1) {
+    articleTitle.push(dataList[i].title);
+    articleSlug.push(dataList[i].slug);
+    articleDate.push(`${dataList[i].date.getDate()}.${dataList[i].date.getMonth() + 1}.${dataList[i].date.getFullYear()}`);
+    articleImg.push(dataList[i].image);
+  }
+  res.render('index', {
+    title: 'Greinasafnið',
+    headerText: 'Greinasafnið',
+    articleTitle,
+    articleSlug,
+    articleDate,
+    articleImg,
+  });
+});
+router.get('/:data', async (req, res) => {
+  let data = await getFilesAsync();
+  data = data.filter(x => x);
+  const slugList = getSlugList(data);
+  const article = req.params.data;
+  for (let i = 0; i < slugList.length; i += 1) {
+    if (article === slugList[i]) {
+      const articleHTML = marked(data[i].body);
+      res.render('article', {
+        title: 'Article',
+        headerText: data[i].attributes.title,
+        article: articleHTML,
+      });
+    }
+  }
+  res.status(404).render('error', {
+    title: 'Villa',
+    headerText: 'Fannst ekki',
+    errorText: 'Ó nei, efnið fannst ekki',
+  });
+});
+
+module.exports = router;
